85f954bd2060cda44f37348f0940659b8858a490
Dennis Vesterlund <dennisvesterlund@gmail.com>
2014-09-23 12:04:09 +0200

    Started addind portlist into readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ fileDamper
 ==========
 
 Yeah, still in development?
+
+Ports used
+==========
+* 37300 Dunno yet
--- make.sh
+++ make.sh
@@ -2,7 +2,21 @@
 #Just a simple script to keep all unesessary files in a diffrent dir
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
+
+case $1 in
+    "debug")
+        echo "Building with debug symbols."
+        cmake -DCMAKE_BUILD_TYPE=Debug ..
+        make
+        ;;
+    *)
+        echo "Building release."
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+        make
+        echo "Stripping binairy"
+        strip fileDamper
+        ;;
+esac
+
 mv fileDamper ../
 cd ..
